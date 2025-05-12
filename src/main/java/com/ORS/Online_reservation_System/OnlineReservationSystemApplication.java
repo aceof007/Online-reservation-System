1ef9fe49d1252f2262c391c92ef43a6a9b9e8673
aceof007 <manuelwilfrid123@gmail.com>
2025-05-12 18:14:42 +0100

    Project structure

---
+++ src/main/java/com/ORS/Online_reservation_System/OnlineReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ORS.Online_reservation_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ORS/Online_reservation_System/OnlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ORS.Online_reservation_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
